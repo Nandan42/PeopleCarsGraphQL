fc7102baf157f66126836f6b321cc75ce51d839e
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-06-28 09:51:42 -0700

    push again

--- server/index.js
+++ server/index.js
@@ -4,25 +4,99 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-let people = [
-  { id: '1', firstName: 'Bill', lastName: 'Gates' },
-  { id: '2', firstName: 'Steve', lastName: 'Jobs' },
-  { id: '3', firstName: 'Linux', lastName: 'Torvalds' }
-];
-
-let cars = [
-  { id: '1', year: 2019, make: 'Toyota', model: 'Corolla', price: 40000, personId: '1' },
-  { id: '2', year: 2018, make: 'Lexus', model: 'LX 600', price: 13000, personId: '1' },
-  { id: '3', year: 2017, make: 'Honda', model: 'Civic', price: 20000, personId: '1' },
-  { id: '4', year: 2019, make: 'Acura', model: 'MDX', price: 60000, personId: '2' },
-  { id: '5', year: 2018, make: 'Ford', model: 'Focus', price: 35000, personId: '2' },
-  { id: '6', year: 2017, make: 'Honda', model: 'Pilot', price: 45000, personId: '2' },
-  { id: '7', year: 2019, make: 'Volkswagen', model: 'Golf', price: 40000, personId: '3' },
-  { id: '8', year: 2018, make: 'Kia', model: 'Sorento', price: 45000, personId: '3' },
-  { id: '9', year: 2017, make: 'Volvo', model: 'XC40', price: 55000, personId: '3' }
-];
-
-// GraphQL schema
+const people = [
+    {
+      id: '1',
+      firstName: 'Bill',
+      lastName: 'Gates'
+    },
+    {
+      id: '2',
+      firstName: 'Steve',
+      lastName: 'Jobs'
+    },
+    {
+      id: '3',
+      firstName: 'Linux',
+      lastName: 'Torvalds'
+    }
+  ]
+  
+  const cars = [
+    {
+      id: '1',
+      year: '2019',
+      make: 'Toyota',
+      model: 'Corolla',
+      price: '40000',
+      personId: '1'
+    },
+    {
+      id: '2',
+      year: '2018',
+      make: 'Lexus',
+      model: 'LX 600',
+      price: '13000',
+      personId: '1'
+    },
+    {
+      id: '3',
+      year: '2017',
+      make: 'Honda',
+      model: 'Civic',
+      price: '20000',
+      personId: '1'
+    },
+    {
+      id: '4',
+      year: '2019',
+      make: 'Acura ',
+      model: 'MDX',
+      price: '60000',
+      personId: '2'
+    },
+    {
+      id: '5',
+      year: '2018',
+      make: 'Ford',
+      model: 'Focus',
+      price: '35000',
+      personId: '2'
+    },
+    {
+      id: '6',
+      year: '2017',
+      make: 'Honda',
+      model: 'Pilot',
+      price: '45000',
+      personId: '2'
+    },
+    {
+      id: '7',
+      year: '2019',
+      make: 'Volkswagen',
+      model: 'Golf',
+      price: '40000',
+      personId: '3'
+    },
+    {
+      id: '8',
+      year: '2018',
+      make: 'Kia',
+      model: 'Sorento',
+      price: '45000',
+      personId: '3'
+    },
+    {
+      id: '9',
+      year: '2017',
+      make: 'Volvo',
+      model: 'XC40',
+      price: '55000',
+      personId: '3'
+    }
+  ]
+  
 const typeDefs = gql`
   type Person {
     id: ID!
@@ -62,18 +136,21 @@ const resolvers = {
     cars: () => cars,
     person: (_, { id }) => people.find(person => person.id === id),
   },
+
   Mutation: {
     addPerson: (_, { firstName, lastName }) => {
       const newPerson = { id: uuidv4(), firstName, lastName };
       people.push(newPerson);
       return newPerson;
     },
+
     updatePerson: (_, { id, firstName, lastName }) => {
       const personIndex = people.findIndex(person => person.id === id);
       if (personIndex === -1) return null;
       people[personIndex] = { ...people[personIndex], firstName, lastName };
       return people[personIndex];
     },
+
     deletePerson: (_, { id }) => {
       const personIndex = people.findIndex(person => person.id === id);
       if (personIndex === -1) return null;
@@ -81,17 +158,20 @@ const resolvers = {
       cars = cars.filter(car => car.personId !== id);
       return deletedPerson;
     },
+
     addCar: (_, { year, make, model, price, personId }) => {
       const newCar = { id: uuidv4(), year, make, model, price, personId };
       cars.push(newCar);
       return newCar;
     },
+
     updateCar: (_, { id, year, make, model, price, personId }) => {
       const carIndex = cars.findIndex(car => car.id === id);
       if (carIndex === -1) return null;
       cars[carIndex] = { ...cars[carIndex], year, make, model, price, personId };
       return cars[carIndex];
     },
+    
     deleteCar: (_, { id }) => {
       const carIndex = cars.findIndex(car => car.id === id);
       if (carIndex === -1) return null;
