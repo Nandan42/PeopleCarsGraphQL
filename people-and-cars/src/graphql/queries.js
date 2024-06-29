330457bc1ac4b1e5109b85eda32dc88b2e6c449e
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-06-29 00:56:37 -0700

    edit and delete functionality addded

--- people-and-cars/src/graphql/queries.js
+++ people-and-cars/src/graphql/queries.js
@@ -68,3 +68,49 @@ export const GET_PERSON_WITH_CARS = gql`
     }
   }
 `;
+
+export const UPDATE_PERSON = gql`
+  mutation UpdatePerson($id: ID!, $firstName: String!, $lastName: String!) {
+    updatePerson(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const DELETE_PERSON = gql`
+  mutation DeletePerson($id: ID!) {
+    deletePerson(id: $id) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const UPDATE_CAR = gql`
+  mutation UpdateCar($id: ID!, $year: Int!, $make: String!, $model: String!, $price: Float!, $personId: String!) {
+    updateCar(id: $id, year: $year, make: $make, model: $model, price: $price, personId: $personId) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`;
+
+export const DELETE_CAR = gql`
+  mutation DeleteCar($id: ID!) {
+    deleteCar(id: $id) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`;
